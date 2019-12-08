7992b174bb7e6a104e6c67bcefccfbff803c7c46
maximerischard <rischardmaxime@gmail.com>
2019-12-08 22:54:05 +0000

    fix path in python plotting code

--- examples/NYC/NYC_prepare_plots.py
+++ examples/NYC/NYC_prepare_plots.py
@@ -9,7 +9,7 @@ plt.interactive(False)
 EPSG=2263 # projection
 import os
 dirname = os.path.dirname(__file__)
-nysd_filename = os.path.join(dirname, "NYC_data/nysd_16c/nysd.shp")
+nysd_filename = os.path.join(dirname, "NYC_data/nysd_16c/nysd.json")
 nycdistrs=gpd.read_file(nysd_filename).to_crs(epsg=EPSG)
 
 def background_schdistrs(ax, **kwargs):
